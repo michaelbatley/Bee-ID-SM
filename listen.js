d77f046e5d7ff4d271d643b6cab8c1cfbb0df6f1
Michael Batley <michael.batley@yahoo.com.au>
2016-10-26 11:40:12 +1100

    Add files via upload

---
+++ listen.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("drawer").addEventListener("click", openFunc);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("sexicon").addEventListener("click", sexChange);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("help").addEventListener("click", openhelp);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("clear").addEventListener("click", clearFunc);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("apply").addEventListener("click", closeFunc);
+});
+
+function openFunc() {
+	var mask = localStorage.getItem("storedmask");
+	document.getElementById('myModal').style.display = "block";
+
+/*	for (i=0; i<15; i++) {
+		if ((mask & Math.pow(2,i))>0) {document.getElementById("par"+(i////+1)).checked=true;}
+	}*/
+};
+
+function closeFunc() {
+	var thismask = 0;
+	for (i=0; i<15; i++) {
+	if (document.getElementById("par"+(i+1)).checked) {
+	thismask = thismask + Math.pow(2,i)
+	}
+   }
+	localStorage.setItem("storedmask", thismask);
+	document.getElementById('myModal').style.display = "none";
+	document.location.reload(true);
+}
+
+function clearFunc() {
+	for (i=0; i<15; i++) {
+		document.getElementById("par"+(i+1)).checked = false;
+		localStorage.setItem("storedmask", 0);
+	}
+}
+
+function sexChange() {
+	var sex = localStorage.getItem("storedsex");
+	if (sex == "m") {localStorage.setItem("storedsex","f");} 
+	if (sex == "f") {localStorage.setItem("storedsex","m");
+ 			document.getElementById("sexicon").src="images/female.png"; }
+	document.location.reload(true);
+}
+
+function openhelp() {
+	document.location.assign("help.htm");
+}
