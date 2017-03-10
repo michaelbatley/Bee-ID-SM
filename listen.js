f30e877dbea8e8022c7b9595c4ef620806f8a89d
Michael Batley <michael.batley@yahoo.com.au>
2017-03-10 15:54:17 +1100

    Add files via upload

--- listen.js
+++ listen.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("drawer").addEventListener("click", openFunc);
+  document.getElementById("flare").addEventListener("click", openFunc);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("sexicon").addEventListener("click", sexChange);
+  document.getElementById("flares").addEventListener("click", sexChange);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("help").addEventListener("click", openhelp);
+  document.getElementById("flareh").addEventListener("click", openhelp);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
